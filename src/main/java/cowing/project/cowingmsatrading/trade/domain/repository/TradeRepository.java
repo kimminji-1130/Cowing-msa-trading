4751f0a05d1f550c9efb9da96d3d8a17137a11eb
ekwls20 <ekwls20@gmail.com>
2025-07-14 16:08:33 +0900

    Refactor(#14): Change findAllByUsername to return Optional<List<Trade>>

--- src/main/java/cowing/project/cowingmsatrading/trade/domain/repository/TradeRepository.java
+++ src/main/java/cowing/project/cowingmsatrading/trade/domain/repository/TradeRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TradeRepository extends JpaRepository<Trade, Long> {
-    List<Trade> findAllByUsername(String username);
+    Optional<List<Trade>> findAllByUsername(String username);
 }
