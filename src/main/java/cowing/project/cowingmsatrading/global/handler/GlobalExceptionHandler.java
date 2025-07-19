2d131f9e450ab488b04099de764258d2133ae7e1
ekwls20 <ekwls20@gmail.com>
2025-07-19 12:35:19 +0900

    Refactor(#26): AuthenticationException import 경로 변경

--- src/main/java/cowing/project/cowingmsatrading/global/handler/GlobalExceptionHandler.java
+++ src/main/java/cowing/project/cowingmsatrading/global/handler/GlobalExceptionHandler.java
@@ -4,7 +4,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.security.sasl.AuthenticationException;
+import javax.naming.AuthenticationException;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
