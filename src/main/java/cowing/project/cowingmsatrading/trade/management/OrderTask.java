2ec18f2183628bf060bf578b2cdf24a7a0e1a7d4
ekwls20 <ekwls20@gmail.com>
2025-07-19 12:07:25 +0900

    Refactor(#26): 사용하지 않는 Lombok annotations 삭제

--- src/main/java/cowing/project/cowingmsatrading/trade/management/OrderTask.java
+++ src/main/java/cowing/project/cowingmsatrading/trade/management/OrderTask.java
@@ -1,8 +1,6 @@
 package cowing.project.cowingmsatrading.trade.management;
 
 import cowing.project.cowingmsatrading.trade.domain.entity.order.Order;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 // 주문 처리를 위해 큐에 적재되는 태스크 객체
 public record OrderTask(Order order) {
